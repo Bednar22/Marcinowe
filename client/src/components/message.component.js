9f11aa123abfc90da2783ce870278b61689ba58c
Maciek Bednarski <mBednar2020@gmail.com>
2020-07-08 18:32:18 +0200

    dodane

--- client/src/components/message.component.js
+++ client/src/components/message.component.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
 
 class Message extends Component {
-    
+  constructor() {
+    super();
+    this.state = {
+      message: "State test",
+    };
+  }
 
-    constructor(){
-        
-        super()
-        this.state = {
-            message: "State test"
-        }
-    }
+  changeMessage() {
+    this.setState({
+      message: `Clicked button hahhah ${this.props.name}`,
+    });
+  }
 
-    changeMessage(){
-        this.setState({
-            
-            message: `Clicked button ${this.props.name}` 
-        })
-    }
-
-    render(){
-        const {name} = this.props
-        return(
-            <div>
-                <h1>{this.state.message}</h1>
-                <button onClick={() => this.changeMessage()}>TestButton {name} </button>
-            </div>
-
-        )
-    }
+  render() {
+    const { name } = this.props;
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+        <button onClick={() => this.changeMessage()}>TestButton {name} </button>
+      </div>
+    );
+  }
 }
 
-export default Message
+export default Message;
