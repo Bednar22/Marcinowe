b3a674597f36a4a5404d6ead49e01f95800e1096
Maciek Bednarski <mBednar2020@gmail.com>
2020-07-08 18:45:40 +0200

    taa

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
-import Message from "./components/message.component"
+import "./App.css";
+import Message from "./components/message.component";
 import Navbar from "./components/navbar.component";
 import CreateUser from "./components/create-user.component";
 import Counter from "./components/counter.component";
-import EventBind from "./components/clickFunction.component"
+import EventBind from "./components/clickFunction.component";
 function App() {
   return (
     <Router>
       <div className="container">
-      <Navbar />
-      <br/>
-      <Route path="/user" component={CreateUser} />
-       <Message  name="Testtttting"/>   
-      
-      <Counter/>
-      <br/>
-        <EventBind/>
+        <Navbar />
+        <br />
+        <br />
+        <Route path="/user" component={CreateUser} />
+        <Message name="Testtttting" />
+
+        <Counter />
+        <br />
+        <EventBind />
       </div>
     </Router>
   );
